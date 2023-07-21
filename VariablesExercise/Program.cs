e5ffb86f02b6c64f61dca3d3802a0fa721037e8c
Katherine Vail <jabberduckygame@gmail.com>
2023-07-21 09:52:25 -0500

    Completed exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string catName = "Zuli";
+            int catAge = 2;
+            char z = 'Z';
+            bool isCute = true;
+            double inch = 21;
+            decimal weight = 8.5m;
+
+            Console.WriteLine($"My cat's name is {catName}. {z} is {catAge} and it is {isCute} that she is cute. She is {inch} inches and weighs {weight}");
         }
     }
 }
